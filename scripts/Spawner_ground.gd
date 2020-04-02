936d6c2f6701912bc66e7ab8b628da6710166da0
Shivpratap Chauhan <shivmsit@gmail.com>
2020-04-02 22:41:14 +0530

    Fix some time ground missing: use only one signal, using multiple signal does not gauranty in which order 2 callbacks will be called

--- scripts/Spawner_ground.gd
+++ scripts/Spawner_ground.gd
@@ -6,17 +6,12 @@ const AMOUNT_TO_FILL_VIEW = 2
 func _ready():
 	for i in range(AMOUNT_TO_FILL_VIEW):
 		spwan_ground()
-		go_next_pos()
 	pass # Replace with function body.
 
 func spwan_ground():
 	var new_ground = scn_ground.instance()
 	new_ground.position = position
 	new_ground.connect("tree_exited", self, "spwan_ground")
-	new_ground.connect("tree_exited", self, "go_next_pos")
-	get_node("container").add_child(new_ground)
-	pass
-	
-func go_next_pos():
 	position += Vector2(GROUND_WIDTH, 0)
+	get_node("container").add_child(new_ground)
 	pass
