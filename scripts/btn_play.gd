f0e4465321477cf07f405fcab4fe4fdc9786a83d
Shivpratap Chauhan <shivmsit@gmail.com>
2020-04-05 23:10:46 +0530

    disable multiple button click

--- scripts/btn_play.gd
+++ scripts/btn_play.gd
@@ -6,6 +6,7 @@ func _ready():
 
 func _on_button_pressed():
 	stage_manager.change_stage(stage_manager.GAME_STAGE)
+	disabled = true
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
