bf48a3b7cf5558e6934c95d7a2b4c6e5fe570d8e
Shivpratap Chauhan <shivmsit@gmail.com>
2020-04-05 22:55:21 +0530

    no need for animaiotn finished signal, just change layer before and after scene changeis working

--- scripts/stage_manager.gd
+++ scripts/stage_manager.gd
@@ -1,26 +1,18 @@
 extends CanvasLayer
 
 const GAME_STAGE = "res://stages/game_stage.tscn"
+
 func _ready():
-	get_node("amin").connect("animation_started", self, "_on_animaiton_started")
-	get_node("amin").connect("animation_finished", self, "_on_animaiton_finished")
-	pass
-	
-func _on_animaiton_started(anim_name):
-	if anim_name == "fade_in":
-		layer = 5
-	pass
-func _on_animaiton_finished(anim_name):
-	if anim_name == "fade_out":
-		layer = 1
+	#get_node("amin").connect("animation_finished", self, "_on_animaiton_finished")
 	pass
 func change_stage(stage_path):
 	#fade to black
+	layer = 5
 	get_node("amin").play("fade_in")
 	yield(get_node("amin"), "animation_finished")
 	#change to another stage
-	get_tree().change_scene(stage_path)	
+	get_tree().change_scene(stage_path)
+	layer = -2
 	#fade from black
-	get_node("amin").play("fade_out")
-
+	#get_node("amin").play("fade_out")
 	pass
